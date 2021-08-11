b708232d3c5ac9f01b9aae49cc7e6de59781b803
blazejkrzak <blazej@m8b.pl>
2021-08-11 15:48:27 +0200

    Test suite: - Start() happy path for connection wait

--- consensus/pandora/pandora_test.go
+++ consensus/pandora/pandora_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/stretchr/testify/assert"
+	"net"
 	"testing"
 	"time"
 )
@@ -14,6 +15,8 @@ var (
 	dummyRpcFunc = DialRPCFn(func(endpoint string) (rpcClient *rpc.Client, err error) {
 		return rpc.Dial(endpoint)
 	})
+
+	ipcTestLocation = "./test.ipc"
 )
 
 func Test_New(t *testing.T) {
@@ -47,10 +50,12 @@ func TestPandora_Start(t *testing.T) {
 		assert.Nil(t, pandoraEngine.chain)
 	})
 
+	makeOrchestratorServer(t)
+
 	t.Run("should wait for connection", func(t *testing.T) {
 		t.Parallel()
 		waitingPandoraEngine := createDummyPandora(t)
-		waitingPandoraEngine.endpoint = dummyEndpoint
+		waitingPandoraEngine.endpoint = ipcTestLocation
 		ticker := time.NewTicker(reConPeriod)
 		dummyError := fmt.Errorf("dummy Error")
 
@@ -59,10 +64,15 @@ func TestPandora_Start(t *testing.T) {
 		}
 
 		waitingPandoraEngine.Start(nil)
+		t.Log("Waiting for reconnection in Pandora Engine")
 		<-ticker.C
 		assert.Equal(t, dummyError, waitingPandoraEngine.runError)
-
 		waitingPandoraEngine.dialRPC = dummyRpcFunc
+		t.Log("Waiting for reconnection in Pandora Engine")
+		<-ticker.C
+		_, server, _ := makeOrchestratorServer(t)
+		defer server.Stop()
+		t.Log("Waiting for reconnection in Pandora Engine")
 		<-ticker.C
 		assert.Equal(t, uint64(0), waitingPandoraEngine.currentEpoch)
 		assert.Nil(t, waitingPandoraEngine.runError)
@@ -79,6 +89,36 @@ func createDummyPandora(t *testing.T) (pandoraEngine *Pandora) {
 	urls := make([]string, 2)
 	dialGrpcFnc := dummyRpcFunc
 	pandoraEngine = New(ctx, cfg, urls, dialGrpcFnc)
+
+	return
+}
+
+type OrchestratorApi struct{}
+
+func (orchestratorApi *OrchestratorApi) MinimalConsensusInfo(
+	ctx context.Context,
+	epoch uint64,
+) (subscription *rpc.Subscription, err error) {
+	return
+}
+
+func makeOrchestratorServer(
+	t *testing.T,
+) (listener net.Listener, server *rpc.Server, location string) {
+	location = ipcTestLocation
+	apis := make([]rpc.API, 0)
+	api := &OrchestratorApi{}
+
+	apis = append(apis, rpc.API{
+		Namespace: "orc",
+		Version:   "1.0",
+		Service:   api,
+		Public:    true,
+	})
+
+	// TODO: change to inproc?
+	listener, server, err := rpc.StartIPCEndpoint(location, apis)
+	assert.NoError(t, err)
 
 	return
 }
