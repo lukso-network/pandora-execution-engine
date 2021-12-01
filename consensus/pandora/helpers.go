7d9b03a6fa92f84ceba288b17b64fa19f6380a19
Muhaimin Anando <anandocse@gmail.com>
2021-12-02 01:25:36 +0600

    Test/handler stalled (#120)
    * handler stalled and removed code from handler start
    
    * all removed
    
    Co-authored-by: muhaimin <muhaimin@cefalo.com>

--- consensus/pandora/helpers.go
+++ consensus/pandora/helpers.go
@@ -76,10 +76,10 @@ func (p *Pandora) verifyHeader(chain consensus.ChainHeaderReader, header, parent
 	if header.Time <= parent.Time {
 		return errOlderBlockTime
 	}
-	if !isAscendingSlot(parent, header) {
-		log.Error("slot numbers are not in ascending order", "prev head blockNumber", parent.Number.Uint64(), "current header blockNumber", header.Number.Uint64())
-		return consensus.ErrInvalidSlotSequence
-	}
+	//if !isAscendingSlot(parent, header) {
+	//	log.Error("slot numbers are not in ascending order", "prev head blockNumber", parent.Number.Uint64(), "current header blockNumber", header.Number.Uint64())
+	//	return consensus.ErrInvalidSlotSequence
+	//}
 	// Verify the block's difficulty based on its timestamp and parent's difficulty
 	expected := p.CalcDifficulty(chain, header.Time, parent)
 	if expected.Cmp(header.Difficulty) != 0 {
